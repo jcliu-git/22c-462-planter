71021af71a5fda01dd2b72735138f105c9148e04
phammichael1001 <13736+phammichael1001@users.noreply.github.tamu.edu>
2022-11-07 11:56:25 -0600

    made changes to communicate to control hub

--- contract/contract.py
+++ contract/contract.py
@@ -74,3 +74,28 @@ class MoistureReadingMessage(Message[IMoistureData]):
         return MoistureReadingMessage(
             message["data"]["moisture"], message["data"]["timestamp"]
         )
+
+# monitoring definitions
+
+class PhotoCapture(TypedDict):
+    filename: float
+    phototype: float
+    timestamp: Optional[datetime.datetime]
+
+class PhotoCaptureMessage(Message[PhotoCapture]):
+    filename: float
+    phototype: float
+    timestamp: Optional[datetime.datetime]
+
+    def __init__(self, filename: float, phototype: float, timestamp: Optional[datetime.datetime] = None):
+        super().__init__(
+            MessageType.DATA,
+            System.CAMERA,
+            {"filename": filename, "phototype": phototype, "timestamp": timestamp},
+        )
+
+    @staticmethod
+    def fromJson(message: IMessage[PhotoCapture]):
+        return PhotoCaptureMessage(
+            message["data"]["filename"], message["data"]["phototype"], message["data"]["timestamp"]
+        )
