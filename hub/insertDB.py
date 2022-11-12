07bef000cdb043cc0fb6d0ef2a0485f4d034f75a
Tyler Chin <84422985+TylerYChin@users.noreply.github.com>
2022-11-12 16:06:18 -0600

    initial functions for inserting into db

--- hub/insertDB.py
+++ hub/insertDB.py
@@ -29,31 +29,19 @@ async def main():
 
 asyncio.run(main())
 
-"""def insertDB():
-    while True:
-        if control.queue:
-            payload = control.queue.pop(0)
-            name = payload['name']
-            type = payload['type']
-            data = payload['data']
-            if name == 'monitor_event' and type =='file': #for now, only monitor_event sends file type
-                filesize = data[0]
-                path = data[1]
-                timestamp = data[2][0]
-                pictype = data[2][1]
-                filename = data[2][2]
-                query = 'INSERT INTO monitor_event VALUES(%s,%s,%s,%s,%s)'
-                curr.execute(query, (timestamp,pictype,filename,filesize,path))
-            else: #data type 
-                print('Inserting data...')
-                print(data)
-                query = 'INSERT INTO ' + name + ' VALUES(%s,%s);'
-                curr.execute(query, (datetime.now(), data))
-                print('done')
-        sleep(1)
-"""
+def insertDB(table: str, cols: str, data: str):
+    curr.execute(f"INSERT INTO {table} {cols} VALUES{data}")
+    conn.commit()
 
-
+def insertMoistureLevel(data):
+    table = "moisture_level"
+    cols = "(sensor_1, sensor_2, sensor_3, sensor_4, sensor_5, sensor_6, sensor_7, sensor_8)"
+    values = "("
+    for i in range(1, 7):
+        values += str(data[i-1])
+        values += ", "
+    values += str(data[7]) + ")"
+    insertDB(table, cols, values)
 
 
 
