4c3c00f95b8b66408b6197321b578d5b6ef78079
CharlesPalmer2 <100155260+CharlesPalmer2@users.noreply.github.com>
2022-11-12 22:01:53 +0000

    camera server communication update

--- monitoring/camera.py
+++ monitoring/camera.py
@@ -1,7 +1,8 @@
 import time
 import sys
 import asyncio
-from aioconsole import ainput
+sys.path.append("../")
+#from aioconsole import ainput
 import contract.contract as contract
 from datetime import date
 from datetime import datetime
@@ -12,10 +13,9 @@ import RPi.GPIO as GPIO
 from hub.network462 import Client
 from time import sleep
 import json
-sys.path.append("../")
 
 # client for file transfer
-client = Client("test", "127.0.0.1", 32132)
+client = Client("test", "192.168.3.114", 32132)
 template_src_path = "source/test"
 template_dest_path = "dest/test"
 
@@ -28,7 +28,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.OUT)
 servo2 = GPIO.PWM(27, 50) # continuous servo
 
-def camera_capture(time, phototype, filename):
+async def camera_capture(time, phototype, filename):
     
     # execute_query(connection, monitor_event)
     camera.start_preview()
@@ -39,9 +39,9 @@ def camera_capture(time, phototype, filename):
     data['time'] = time
     data['type'] = phototype
     data['filename'] = filename
-    monitor_event = json.dumps(data)
-    return monitor_event
+    
     #await client.sendFile(filename+ ".jpg", filename+ ".jpg")
+    return data
 
 def motion_track():
     # Defined angles of rotation for servo2(continuous servo)
@@ -63,19 +63,19 @@ async def main():
         cooldown = False
         current_date = time.strftime("%b-%d-%Y")
         db_time = datetime.now()
-        current_time = time.strftime("%H:%M:%S", t)
+        current_time = time.strftime("%H-%M-%S", t)
         if cooldown == False:
-            if pir.motion_detected():  # takes picture if detects motion
+            if pir.motion_detected:  # takes picture if detects motion
                 filename = current_date + "_motion_image_" + current_time
-                monitor_event = camera_capture(db_time, "motion", filename)
-                await client.sendData(monitor_event)
-                await client.sendFile(filename+ ".jpg", filename+ ".jpg")
+                monitor_event = await camera_capture(current_date, "motion", filename)
+                #await client.sendData(monitor_event)
+                await client.sendFile(filename+ ".jpg", filename+ ".jpg", monitor_event)
                 cooldown = True
             if time.strftime("%M:%S", t) == "30:00":  # takes pictures at periods
                 filename = current_date + "_time_image_" + current_time
                 monitor_event = camera_capture(db_time, "periodic", filename)
-                await client.sendData(monitor_event)
-                await client.sendFile(filename+ ".jpg", filename+ ".jpg")
+                #await client.sendData(monitor_event)
+                await client.sendFile(filename+ ".jpg", filename+ ".jpg",monitor_event)
                 cooldown = True
         if cooldown == True:  # adds a 5 minute cooldown for pictures
             time.sleep(300)
