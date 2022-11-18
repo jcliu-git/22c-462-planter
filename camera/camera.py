3db05eb339ad47cac9bce656073b90980980708d
phamM302 <phammichael1001@tamu.edu>
2022-11-18 06:37:23 -0600

    added motion controls, plant growth picture, and removed visual motion detection

--- camera/camera.py
+++ camera/camera.py
@@ -1,7 +1,7 @@
 import time
 import sys
 import asyncio
-
+import pigpio
 sys.path.append("../")
 # from aioconsole import ainput
 import contract.contract as contract
@@ -12,6 +12,7 @@ import picamera.array
 from gpiozero import MotionSensor
 from gpiozero import Servo
 import RPi.GPIO as GPIO
+import cv2
 from hub.network462 import Client
 from time import sleep
 import numpy as np
@@ -26,28 +27,12 @@ template_dest_path = "dest/test"
 pir = MotionSensor(4)
 camera = PiCamera()
 # servos setup
-servo1 = Servo(17)  # 180 servo
+servo180 = 17 #180 servo
+servo360 = 27 #360 servo
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.OUT)
 servo2 = GPIO.PWM(27, 50)  # continuous servo
-
-class DetectMotion(picamera.array.PiMotionAnalysis):
-    async def analyze(self, a):
-        t = time.localtime()
-        current_date = time.strftime("%b-%d-%Y")
-        current_time = time.strftime("%H-%M-%S", t)
-        a = np.sqrt(
-            np.square(a['x'].astype(np.float)) +
-            np.square(a['y'].astype(np.float))
-            ).clip(0, 255).astype(np.uint8)
-        # If there're more than 5 vectors with a magnitude greater
-        # than 30, then say we've detected motion
-        if (a > 30).sum() > 5:
-            filename = current_date + "_motion_image_" + current_time
-            monitor_event = await camera_capture(current_date, "motion", filename)
-            # await client.sendData(monitor_event)
-            await client.sendFile(filename + ".jpg", monitor_event)
-
+pfile = "temp"
 
 async def camera_capture(time, phototype, filename):
 
@@ -73,6 +58,71 @@ def motion_track():
     left_45 = 10
     right_90 = 12.5
 
+#servo definitions
+
+async def up():
+    servo180.set_mode(servo180,pigpio.OUTPUT)
+    servo180.set_PWM_frequency(17, 50)
+    servo180.set_servo_pulsewidth(17, 1000)
+    time.sleep(3)
+    servo180.set_PWM_dutycycle( 17, 0 )
+    servo180.set_PWM_frequency( 17, 0 )
+
+async def center():
+    servo180.set_mode(servo180,pigpio.OUTPUT)
+    servo180.set_PWM_frequency(17, 50)
+    servo180.set_servo_pulsewidth(17, 1500)
+    time.sleep(3)
+    servo180.set_PWM_dutycycle( 17, 0 )
+    servo180.set_PWM_frequency( 17, 0 )
+
+async def down():
+    servo180.set_mode(servo180,pigpio.OUTPUT)
+    servo180.set_PWM_frequency(17, 50)
+    servo180.set_servo_pulsewidth(17, 2000)
+    time.sleep(3)
+    servo180.set_PWM_dutycycle( 17, 0 )
+    servo180.set_PWM_frequency( 17, 0 )
+
+async def right90():
+    servo360 = pigpio.pi()
+    servo360.set_mode(27, pigpio.OUTPUT)
+    servo360.set_PWM_frequency(27, 50)
+    servo360.set_servo_pulsewidth(27,700)
+    time.sleep(.16)
+    servo360.set_PWM_dutycycle( 27, 0 )
+    servo360.set_PWM_frequency( 27, 0 )
+    time.sleep(1)
+    
+async def right45():
+    servo360 = pigpio.pi()
+    servo360.set_mode(27, pigpio.OUTPUT)
+    servo360.set_PWM_frequency(27, 50)
+    servo360.set_servo_pulsewidth(27,700)
+    time.sleep(.08)
+    servo360.set_PWM_dutycycle( 27, 0 )
+    servo360.set_PWM_frequency( 27, 0 )
+    time.sleep(1)
+    
+async def left45():
+    servo360 = pigpio.pi()
+    servo360.set_mode(27, pigpio.OUTPUT)
+    servo360.set_PWM_frequency(27, 50)
+    servo360.set_servo_pulsewidth(27,1700)
+    time.sleep(.08)
+    servo360.set_PWM_dutycycle( 27, 0 )
+    servo360.set_PWM_frequency( 27, 0 )
+    time.sleep(1)
+    
+async def left90():
+    servo360 = pigpio.pi()
+    servo360.set_mode(27, pigpio.OUTPUT)
+    servo360.set_PWM_frequency(27, 50)
+    servo360.set_servo_pulsewidth(27,1750)
+    time.sleep(.16)
+    servo360.set_PWM_dutycycle( 27, 0 )
+    servo360.set_PWM_frequency( 27, 0 )
+    time.sleep(1)
 
 async def main():
     # client for file transfer
@@ -85,13 +135,9 @@ async def main():
         db_time = datetime.now()
         current_time = time.strftime("%H-%M-%S", t)
         if cooldown == False:
-            with picamera.PiCamera() as camera1:
-                with DetectMotion(camera) as output:
-                    camera1.resolution = (640, 480)
-                    camera1.start_recording(
-                        '/dev/null', format='h264', motion_output=output)
-                    camera1.wait_recording(30)
-                    camera1.stop_recording()
+            await center()
+            camera.capture("temp.jpg")
+            sleep(2)
             if pir.motion_detected:  # takes picture if detects motion
                 filename = current_date + "_motion_image_" + current_time
                 monitor_event = await camera_capture(current_date, "motion", filename)
@@ -99,10 +145,20 @@ async def main():
                 await client.sendFile(filename + ".jpg", monitor_event)
                 cooldown = True
             if time.strftime("%M:%S", t) == "30:00":  # takes pictures at periods
+                await down()
+                sleep(2)
                 filename = current_date + "_time_image_" + current_time
-                monitor_event = camera_capture(db_time, "periodic", filename)
+                monitor_event = await camera_capture(db_time, "periodic", filename)
                 # await client.sendData(monitor_event)
                 await client.sendFile(filename + ".jpg", monitor_event)
+                #checks for plant growth weekly (sunday)
+                if time.strftime("%w:%H:%M:%S",t) == "0:12:00:00":
+                    plant1 = cv2.imread(filename +  ".jpg" )
+                    plant2 = cv2.imread(pfile + ".jpg")
+                    plantdifference = cv2.subtract(plant1, plant2)
+                    cv2.imwrite(filename + "_growth.jpg" , plantdifference)
+                    await client.sendFile(filename + "_growth.jpg", monitor_event)
+                    pfile = filename
                 cooldown = True
         if cooldown == True:  # adds a 5 minute cooldown for pictures
             time.sleep(300)
