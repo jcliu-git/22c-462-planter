2b121c03c7e0adca819142b25b48943dd752c822
Tyler Chin <84422985+TylerYChin@users.noreply.github.com>
2022-11-27 17:20:03 -0600

    modify startup sequence to use services instead

--- hub/startup.sh
+++ hub/startup.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-# launcher.sh
-# navigate to home directory, then to this directory, then execute python script, then back home
 
-cd /
-cd home/pi/22c-462-planter/hub
-python3 hub.py
-cd /home/
+python ./hub.py
